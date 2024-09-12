78025af759e4ebf7209c6c99a2e5c76f6d2dd25b
tomi1212 <na08ru11to30@gmail.com>
2024-09-12 21:53:31 +0900

    トップページ更新

---
+++ header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+        <title>紀尾井町プラザクリニック</title>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style.css">
+        <?php wp_head(); ?>
+</head> 
+<body>
+  <header class="custom-header">
+    <div class="header-logo">Progate</div>
+    <div class="header-list">
+        <ul>
+            <li>プログラミングとは</li>
+            <li>学べるレッスン</li>
+            <li>お問い合わせ</li>
+       </ul>
+    </div>
+  </header>
