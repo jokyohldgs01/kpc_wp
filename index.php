f24909ed74d68dc24356ab6459f3b692fbf92aff
jokyohldgs01 <jokyohldgs01@gmail.com>
2024-09-12 19:09:34 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  // ******************************
+  // index.php
+  // ******************************
+?>
+<h1>index.php</h1>
+
+<?php get_header(); ?>
+<?php get_template_part('include/breadcrumb'); ?>
+<?php get_footer(); ?>
