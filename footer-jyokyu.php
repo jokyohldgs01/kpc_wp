589c02f3e8de6899e5878de4774166b997091c55
tomi1212 <na08ru11to30@gmail.com>
2024-10-01 15:10:23 +0900

    一旦最新化

---
+++ footer-jyokyu.php
@@ -0,0 +1,8 @@
+<footer>
+      <div class="container">
+        <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
+        <p>Learn to code, learn to be creative.</p>
+      </div>
+    </footer>
+  </body>
+</html>
---
+++ header-jyokyu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+        <title>紀尾井町プラザクリニック</title>
+        <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/css/style-jyokyu.css">
+        <?php wp_head(); ?>
+</head> 
+<body>
+  <header>
+    <div class="container">
+      <div class="header-left">
+        <img class="logo" src="https://prog-8.com/images/html/advanced/main_logo.png">
+      </div>
+
+      <span class="fa fa-bars menu-icon"></span>
+
+      <div class="header-right">
+        <a href="#" class="login">ログイン</a>
+       </div>
+    </div>
+  </header>
