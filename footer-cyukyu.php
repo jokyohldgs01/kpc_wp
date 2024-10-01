a708b82779b33ad17ce7e86b4c752f0831cca65b
jokyohldgs01 <jokyohldgs01@gmail.com>
2024-10-01 15:30:45 +0900

    master先行更新

--- footer-cyukyu.php
+++ footer-cyukyu.php
@@ -1,7 +1,7 @@
 <footer>
       <div class="container">
         <img src="https://prog-8.com/images/html/advanced/footer_logo.png">
-        <p>Learn to code, learn to be creative.</p>
+        <p>Learn to code, learn to be creative.マスターを先行して更新</p>
       </div>
     </footer>
   </body>
